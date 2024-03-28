61b607cd45c805807091e628c8fa3b2cb0a77f41
Duygu Aras <109031940+DuyguAras@users.noreply.github.com>
2024-03-28 13:29:40 +0300

    Add: Symbol Prefabs

--- Assets/Game/Scripts/Slot/SymbolManager.cs
+++ Assets/Game/Scripts/Slot/SymbolManager.cs
@@ -6,13 +6,23 @@ namespace CoreGames.GameName
 {
     public class SymbolManager : MonoBehaviour
     {
-        // Start is called before the first frame update
+        [SerializeField] private int verticalHeight;
+        [SerializeField] private int horizontalHeight;
+
+        [SerializeField] private List<GameObject> symbols;
+
         void Start()
         {
-        
+            for (int x = 0; x < horizontalHeight; x++)
+            {
+                for (int y = 0; y < verticalHeight; y++)
+                {
+                    GameObject symbol = Instantiate(symbols[0], new Vector2(x,y), Quaternion.identity);
+                }
+            }
         }
 
-        // Update is called once per frame
+        
         void Update()
         {
         
